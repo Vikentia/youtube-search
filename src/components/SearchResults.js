23667c95608144444613eae2f4950036b037e3ba
Victoria Kovaliova <victkovaliova@gmail.com>
2022-04-12 13:33:44 +0300

    update

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,19 +1,40 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 
+import list from '../assets/image/list.svg';
+import grid from '../assets/image/grid.svg';
 import './SearchResults.css';
 
 function SearchResults({ resultsVideos, valueRequest }) {
+
+    const [sort, setSort] = useState('list');
+
+
     return (
+
         <div className='wrap'>
             {
-                valueRequest ? <p> Видео по запросу: &#171;{valueRequest}&#187;</p> : null
+                valueRequest
+                    ? <div className='filter-panel'>
+                        <p> Видео по запросу: &#171;{valueRequest}&#187;</p>
+                        <div className='video-sort'>
+                            <button onClick={() => setSort('list')}>
+                                <img className={sort == 'grid' ? 'sort-list sort-opacity' : 'sort-list'} src={list} alt="list" />
+                            </button>
+                            <button onClick={() => setSort('grid')}>
+                                <img className={sort == 'list' ? 'sort-grid sort-opacity' : 'sort-grid'} src={grid} alt="grid" />
+                            </button>
+                        </div>
+                    </div>
+                    : null
+
             }
             <div className='wrap-video'>
                 {
                     valueRequest
                     && resultsVideos.map(item => {
                         let link = `https://www.youtube.com/embed/${item.id.videoId}`;
-                        console.log(item);
+                        // console.log(item);
                         return (
                             <div className={'video-item'}>
                                 <iframe className={'video'} src={link} > </iframe>
@@ -25,7 +46,8 @@ function SearchResults({ resultsVideos, valueRequest }) {
                 }
             </div>
         </div >
-    );
+
+    )
 }
 
 export default SearchResults;
